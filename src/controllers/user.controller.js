242c486437c17d368047b91a0f72e4414ca64d54
Amrit Kumar <amrit24680gg@gmail.com>
2024-11-15 20:20:41 +0530

    added refreshAccessToken route

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,17 +3,25 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import mongoose from "mongoose";
+import jwt from 'jsonwebtoken'
 
-
-const generateAccessAndRefereshTokens=async(userId)=>{
+const generateAccessAndRefreshTokens=async(userId)=>{
   try {
-    const user=User.findById(userId);
-    const accessToken =user.generateAccessToken();
-    const refreshToken=user.generateRefreshToken();
+    const user=await User.findById(userId);
+    
+    if (!user) {
+      throw new ApiError(
+        500,
+        "Something went wrong while generating Refresh & Access Token due to user not found"
+      );
+    }
+    const accessToken =await user.generateAccessToken();
+    const refreshToken=await user.generateRefreshToken();
 
     user.refreshToken= refreshToken;
 
-    await user.save(validateBeforeSave: false);
+    await user.save({ validateBeforeSave: false });
 
     return {accessToken,refreshToken}
 
@@ -21,6 +29,9 @@ const generateAccessAndRefereshTokens=async(userId)=>{
     throw new ApiError(500, "Something went wrong while generating referesh and access token")
   }
 }
+
+
+
 
 
 const registerUser = asyncHandler(async (req, res) => {
@@ -108,6 +119,7 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdUser, "User registered successfully"));
 });
 
+
 const loginUser=asyncHandler(async(req,res)=>{
 
     // req body -> data
@@ -121,7 +133,7 @@ const loginUser=asyncHandler(async(req,res)=>{
 
   const {email,username,password}=req.body;
 
-  if (!email || !username) {
+  if (!(email || username)) {
     throw new ApiError(400,"username or email is required");
   }
 
@@ -139,9 +151,9 @@ const loginUser=asyncHandler(async(req,res)=>{
     throw new ApiError(401, "Invalid user credentials")
   }
 
-  const {accessToken,refreshToken}=await generateAccessAndRefereshTokens(user._id)
+  const {accessToken,refreshToken}=await generateAccessAndRefreshTokens(user._id)
 
-  const loggedInUser=User.findById(user._id).select("-password -refreshToken");
+  const loggedInUser=await User.findById(user._id).select("-password -refreshToken");
 
   const options={
     httpOnly: true,
@@ -164,6 +176,7 @@ const loginUser=asyncHandler(async(req,res)=>{
     )
   )
 });
+
 
 const logoutUser=asyncHandler(async(req,res)=>{
   await User.findByIdAndUpdate(
@@ -189,8 +202,55 @@ const logoutUser=asyncHandler(async(req,res)=>{
   .clearCookie("refreshToken", options)
   .json(new ApiResponse(200, {},"User logged Oug"))
 });
+
+
+
+const refereshAccessToken=asyncHandler(async(req,res)=>{
+  const incomingRefreshToken=req.cookies.refreshToken || req.body.refreshToken
+
+  if(!incomingRefreshToken){
+    throw new ApiError(401,"Unauthorized request");
+  }
+
+  try {
+    const decodedToken=jwt.verify(incomingRefreshToken,process.env.REFRESH_TOKEN_SECRET)
+    
+    const user=await User.findById(decodedToken?._id)
+  
+    if (!user) {
+      throw new ApiError(401,"invalid refresh token")
+    }
+  
+    if (incomingRefreshToken !== user?.refreshToken) {
+      throw new ApiError(401,"refresh token expired or used") 
+    }
+  
+    const options={
+      httpOnly: true,
+      secure: true,
+    }
+  
+    const {accessToken,newRefreshToken} =await generateAccessAndRefreshTokens(user._id)
+  
+    return res
+    .status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",newRefreshToken,options)
+    .json(
+      new ApiResponse(
+        200,
+        {accessToken, refreshToken: newRefreshToken },
+        "accessToken refreshed successfully"
+      )
+    )
+  } catch (error) {
+    throw new ApiError(401,error?.message ||"invalid refresh Token")
+  }
+})
+
 export { 
   registerUser,
   loginUser,
-  logoutUser
+  logoutUser,
+  refereshAccessToken
  };
